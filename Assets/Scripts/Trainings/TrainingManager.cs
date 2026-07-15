8c187c36b9630a1c6839b8972e540336dfa8dc8e
trafeh <moudatrafeh14@gmail.com>
2026-07-15 18:08:13 +0100

    99% scene 3 done xd

--- Assets/Scripts/Trainings/TrainingManager.cs
+++ Assets/Scripts/Trainings/TrainingManager.cs
@@ -1,6 +1,10 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Collections;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion.Movement;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion.Teleportation;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion.Turning;
 
 public class TrainingManager : MonoBehaviour
 {
@@ -20,9 +24,24 @@ public class TrainingManager : MonoBehaviour
     public GameObject bracelet;
     public GameObject ring;
 
+    [Header("Locomotion")]
+    public ContinuousMoveProvider moveProvider;
+    public TeleportationProvider teleportProvider;
+    public SnapTurnProvider snapTurnProvider;
+
     //======================================================
     // SAFE WALKWAY
     //======================================================
+    
+    private void Start()
+    {
+        if (moveProvider != null)
+            moveProvider.enabled = true;
+
+        if (teleportProvider != null)
+            teleportProvider.enabled = true;
+
+    }
 
     public void LeftSafeZone()
     {
@@ -71,17 +90,19 @@ public class TrainingManager : MonoBehaviour
     // PPE FINISHED
     //======================================================
 
-public void PPECompleted()
-{
-    Debug.Log("PPE Completed!");
+    public void PPECompleted()
+    {
+        Debug.Log("PPE Completed!");
 
-    taskCanvas.PPEFinished();
+        taskCanvas.PPEFinished();
 
-    if (weldingArea != null && ring != null && bracelet != null)
-        weldingArea.SetActive(true);
-        ring.SetActive(true);
-        bracelet.SetActive(true);
-}
+        if (weldingArea != null && ring != null && bracelet != null)
+        {
+            weldingArea.SetActive(true);
+            ring.SetActive(true);
+            bracelet.SetActive(true);
+        }
+    }
 
     //======================================================
     // TRAINING FINISHED
@@ -90,6 +111,13 @@ public void PPECompleted()
     public void TrainingCompleted()
     {
         Debug.Log("TRAINING COMPLETED!");
+
+        if (moveProvider != null)
+            moveProvider.enabled = false;
+
+        if (teleportProvider != null)
+            teleportProvider.enabled = false;
+
 
         if (diactivateWhenWin != null)
             diactivateWhenWin.SetActive(false);
